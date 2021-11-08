138a4ec9b87e6612a07941d64b8c16ea8f96f6c9
simon <simonheiss87@gmail.com>
2021-11-08 18:59:22 +0100

    stuff right

--- battle-mate.Tests/Tests.cs
+++ battle-mate.Tests/Tests.cs
@@ -26,5 +26,36 @@ namespace battle_mate.Tests
             Assert.AreEqual(4, toWOund.FailedRolls);
             Assert.AreEqual(7, toWOund.RawResults.Count);
         }
+        
+        [Test]
+        public void TestArmorSave()
+        {
+            var dice = new Dice(123);
+            var rest = dice.ToHit(12, 6, 4);
+
+            var toWOund = dice.ToWound(3, rest);
+            var armorSave = dice.ArmorSave(3, toWOund);
+
+            Assert.AreEqual(3, armorSave.DiceGoal);
+            Assert.AreEqual(1, armorSave.SucessfullRolls);
+            Assert.AreEqual(2, armorSave.FailedRolls);
+            Assert.AreEqual(3, armorSave.RawResults.Count);
+        }
+        
+        [Test]
+        public void TestWardSave()
+        {
+            var dice = new Dice(123);
+            var rest = dice.ToHit(12, 6, 4);
+
+            var toWOund = dice.ToWound(3, rest);
+            var armorSave = dice.ArmorSave(3, toWOund);
+            var wardSave = dice.ArmorSave(3, armorSave);
+
+            Assert.AreEqual(3, wardSave.DiceGoal);
+            Assert.AreEqual(1, wardSave.SucessfullRolls);
+            Assert.AreEqual(0, wardSave.FailedRolls);
+            Assert.AreEqual(1, wardSave.RawResults.Count);
+        }
     }
 }
