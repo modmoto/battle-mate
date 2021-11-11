423616c53a86f4e39f89258da6b8e69082f2e3ec
simon <simonheiss87@gmail.com>
2021-11-11 23:32:31 +0100

    added things

---
+++ battle-mate/Pages/BattleResult/PointStrings.cs
@@ -0,0 +1,14 @@
+﻿namespace battle_mate.Pages.BattleResult
+{
+    public class PointStrings
+    {
+        public int Points { get; }
+        public string Text { get; }
+
+        public PointStrings(int points, string text)
+        {
+            Points = points;
+            Text = text;
+        }
+    }
+}
