c3138bcd8565af65f675525bdb25bc9306c204a7
simon <simonheiss87@gmail.com>
2021-11-20 14:34:11 +0100

    expl with things

--- battle-mate/Pages/Dices/DiceResultGroup.cs
+++ battle-mate/Pages/Dices/DiceResultGroup.cs
@@ -8,12 +8,10 @@ namespace battle_mate.Pages
         public DiceResultGroup(int min, List<int> results)
         {
             Min = min;
-            BiggerThanAmount = results.Count(r => r >= min);
-            SmallerThanAmount = results.Count(r => r <= min);
+            Amount = results.Count(r => r == min);
         }
 
         public int Min { get; }
-        public int BiggerThanAmount { get; }
-        public int SmallerThanAmount { get; }
+        public int Amount { get; }
     }
 }
