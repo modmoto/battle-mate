b6e3466c5266ac8d02c48e1a79dff4b37d0945cd
awo <awo@test.de>
2022-02-22 13:02:02 +0100

    smal ui fix and convienience

--- battle-mate/Pages/Dices/DiceState.cs
+++ battle-mate/Pages/Dices/DiceState.cs
@@ -7,6 +7,6 @@ namespace battle_mate
     {
         public List<DiceMessage> DiceMessages { get; } = new();
         public int? DiceAmount { get; set; }
-        public int SelectedDice { get; set; } = 6;
+        public int? SelectedDice { get; set; } = 6;
     }
 }
